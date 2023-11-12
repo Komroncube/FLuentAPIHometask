6aade180a0b82dc48131077cbab58b48984d7259
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-12 18:11:24 +0500

    extension for middleware

---
+++ FLuentAPI/ExtensionMiddlewares/MapControllerMiddleware.cs
@@ -0,0 +1,13 @@
+﻿using FLuentAPI.Middlewares;
+
+namespace FLuentAPI.ExtensionMiddlewares
+{
+    public static class MapControllerMiddleware
+    {
+        public static IApplicationBuilder ConfigureRedirectionMap(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RequestMiddleware>();
+            return app;
+        }
+    }
+}
