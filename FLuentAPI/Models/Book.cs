fee556d63b7bb2084d61353f9b528e804c9af67a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-14 15:17:30 +0500

    lazy loading service in book model

--- FLuentAPI/Models/Book.cs
+++ FLuentAPI/Models/Book.cs
@@ -1,10 +1,23 @@
-﻿namespace FLuentAPI.Models;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace FLuentAPI.Models;
 
 public class Book
 {
+    private readonly ILazyLoader _loader;
+
+    public Book() { }
+    public Book(ILazyLoader loader)
+    {
+        _loader = loader;
+    }
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public int AuthorId { get; set; }
-    public Author Author { get; set; }
+    private Author? _author;
+    public Author Author {
+        get => _loader.Load(this, ref _author);
+        set => _author = value;
+    }
 }
