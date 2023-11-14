cf5096f24c5121a69a2d161e671b30ec5fc086d4
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-14 15:18:12 +0500

    token service to get tokens

---
+++ FLuentAPI/Services/TokenServices/ITokenService.cs
@@ -0,0 +1,7 @@
+﻿namespace FLuentAPI.Services.TokenServices
+{
+    public interface ITokenService
+    {
+        string GenerateToken(string username);
+    }
+}
---
+++ FLuentAPI/Services/TokenServices/TokenService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FLuentAPI.Services.TokenServices;
+
+public class TokenService : ITokenService
+{
+    public string GenerateToken(string username)
+    {
+        var claims = new Claim[]
+        {
+            // name
+            new Claim(JwtRegisteredClaimNames.Name, username),
+            // identifikator
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            // vaqti
+            new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+        };
+        var token = new JwtSecurityToken(
+            claims:claims,
+            expires: DateTime.UtcNow.AddMinutes(1)
+            );
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        return tokenHandler.WriteToken(token);
+
+    }
+}
