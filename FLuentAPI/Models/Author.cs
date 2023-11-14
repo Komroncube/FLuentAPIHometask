4b8e78451e87216e48fbe7ee048bef849e258dd4
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-14 15:17:11 +0500

    lazy loading for data navigations in author model

--- FLuentAPI/Models/Author.cs
+++ FLuentAPI/Models/Author.cs
@@ -1,11 +1,19 @@
-﻿namespace FLuentAPI.Models;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace FLuentAPI.Models;
 
 public class Author
 {
+    private readonly ILazyLoader _loader;
+
     public int Id { get; set; }
     public string FullName { get; set; }
     public decimal Salary { get; set; }
-    public ICollection<Book> Books { get; set; }
+    private ICollection<Book> _books;
+    public ICollection<Book> Books {
+        get => _loader.Load(this, ref _books);
+        set => _books = value;
+    }
     public Author()
     {
         
@@ -15,5 +23,9 @@ public class Author
         Id = id;
         FullName = fullname;
         Salary = salary;
+    }
+    public Author(ILazyLoader loader)
+    {
+        _loader = loader;
     }
 }
