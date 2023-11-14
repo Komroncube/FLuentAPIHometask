d014dc2e29017c08094962a17b329b3c91f01ebd
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-14 15:21:43 +0500

    configuration for jwt

--- FLuentAPI/Program.cs
+++ FLuentAPI/Program.cs
@@ -1,6 +1,13 @@
 using FLuentAPI.DataContext;
 using FLuentAPI.ExtensionMiddlewares;
 using FLuentAPI.Middlewares;
+using FLuentAPI.Services.AuthServices;
+using FLuentAPI.Services.TokenServices;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Text.Json.Serialization;
 
 namespace FLuentAPI;
 
@@ -11,13 +18,73 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        //    .AddJwtBearer(options=>
+        //    {
+        //        options.TokenValidationParameters = new TokenValidationParameters
+        //        {
+        //            //kim tomonidan berildi
+        //            ValidateIssuer = true,
+        //            //kimga berildi
+        //            ValidateAudience = true,
+        //            //qancha muddatga berildi
+        //            ValidateLifetime = true,
+        //            //secret keyni tekshirish
+        //            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        //            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]))
+        //        };
+        //    });
 
         builder.Services.AddControllers();
+        builder.Services.AddScoped<IAuthService, AuthService>();
+        builder.Services.AddSingleton<ITokenService, TokenService>();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
+
+        //builder.Services.AddSwaggerGen(options =>
+        //{
+        //    options.SwaggerDoc("V1", new OpenApiInfo
+        //    {
+        //        Version = "v1",
+        //        Title = "BookstoreAuth",
+
+        //    });
+
+        //    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        //    {
+        //        Scheme = "Beared",
+        //        BearerFormat = "JWT",
+        //        Type = SecuritySchemeType.Http
+        //    });
+
+        //    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        //    {
+        //        {
+        //            new OpenApiSecurityScheme()
+        //            {
+        //                Reference = new OpenApiReference()
+        //                {
+        //                    Id = "Bearer",
+        //                    Type = ReferenceType.SecurityScheme
+        //                }
+        //            },
+        //            new List<string>()
+        //        }
+        //    });
+        //});
+
         builder.Services.AddSwaggerGen();
+        builder.Services.AddControllersWithViews()
+                        .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
         builder.Services.AddDbContext<BookstoreDBContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+
+
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -29,10 +96,11 @@ public class Program
 
         app.UseHttpsRedirection();
 
-        //app.UseAuthorization();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-        //app.MapControllers();
-        app.ConfigureRedirectionMap();
+        app.MapControllers();
+        //app.ConfigureRedirectionMap();
 
         app.Run();
     }
