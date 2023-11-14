13729ffbcda5e5253d087caa90abef3ad1ace0ce
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-14 15:16:12 +0500

    checking username and password service then generate token

---
+++ FLuentAPI/Services/AuthServices/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using FLuentAPI.DataContext;
+using FLuentAPI.Models;
+using FLuentAPI.Services.TokenServices;
+
+namespace FLuentAPI.Services.AuthServices
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ITokenService _tokenService;
+        private readonly BookstoreDBContext _dbContext;
+
+        public AuthService(ITokenService tokenService, BookstoreDBContext dbContext)
+        {
+            _tokenService = tokenService;
+            _dbContext = dbContext;
+        }
+
+        public async ValueTask<string> Login(LoginRequest loginRequest)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Username == loginRequest.Username);
+            if (user is null)
+            {
+                throw new Exception("User not found");
+            }
+            if (user.PasswordHash != loginRequest.Password)
+                throw new Exception("Password is wrong");
+            return _tokenService.GenerateToken(loginRequest.Username);
+        }
+    }
+}
---
+++ FLuentAPI/Services/AuthServices/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using FLuentAPI.Models;
+
+namespace FLuentAPI.Services.AuthServices
+{
+    public interface IAuthService
+    {
+        ValueTask<string> Login(LoginRequest loginRequest);
+    }
+}
