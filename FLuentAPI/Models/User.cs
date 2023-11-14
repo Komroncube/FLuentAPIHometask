c239697dedb92644527bb153d482479ad662de7f
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-14 15:19:21 +0500

    login and user models, for authentification

---
+++ FLuentAPI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FLuentAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
