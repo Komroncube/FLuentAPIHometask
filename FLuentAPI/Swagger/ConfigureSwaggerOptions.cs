5d619b0142c50b30e4c42478645782701ccf512e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-14 21:52:56 +0500

    extracting to another class

---
+++ FLuentAPI/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace FLuentAPI.Swagger
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        public void Configure(SwaggerGenOptions options)
+        {
+            options.SwaggerDoc("V1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "BookstoreAuth",
+                Description = "Auth test desc"
+
+            });
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                Description = "JWT test desc",
+                Type = SecuritySchemeType.Http
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme()
+                    {
+                        Reference = new OpenApiReference()
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        }
+                    },
+                    new List<string>()
+                 
+                }
+            });
+        }
+    }
+}
