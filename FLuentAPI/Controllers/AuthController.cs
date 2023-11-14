36659a8b66a532a363ccfed758b574e367ca679a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-14 15:17:42 +0500

    auth controller to login

---
+++ FLuentAPI/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using FLuentAPI.Models;
+using FLuentAPI.Services.AuthServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FLuentAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> Login(LoginRequest loginRequest)
+        {
+            var token = await _authService.Login(loginRequest);
+            return Ok(token);
+        }
+    }
+}
