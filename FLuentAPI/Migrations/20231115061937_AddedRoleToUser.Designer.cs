f32bccba5aa4704e71c586f412a57e786b08bd16
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-15 11:47:42 +0500

    database uchun migration qo'shildi

--- FLuentAPI/Migrations/20231115061937_AddedRoleToUser.Designer.cs
+++ FLuentAPI/Migrations/20231115061937_AddedRoleToUser.Designer.cs
@@ -3,6 +3,7 @@ using FLuentAPI.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FLuentAPI.Migrations
 {
     [DbContext(typeof(BookstoreDBContext))]
-    partial class BookstoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231115061937_AddedRoleToUser")]
+    partial class AddedRoleToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
