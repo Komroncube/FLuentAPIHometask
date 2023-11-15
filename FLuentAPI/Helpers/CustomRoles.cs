a435a9aa2227c17c817294a9cd7f1a31e48719bb
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-15 11:47:56 +0500

    custom roles for api

---
+++ FLuentAPI/Helpers/CustomRoles.cs
@@ -0,0 +1,15 @@
+﻿namespace FLuentAPI.Helpers
+{
+    public static class CustomRoles
+    {
+        private const string SuperAdmin = "SuperAdmin";
+        private const string Admin = "Admin";
+        private const string Mentor = "Mentor";
+        private const string User = "User";
+
+        public const string AllRoles = SuperAdmin + "," + Admin + "," + Mentor + "," + User;
+        public const string AdminRoles = SuperAdmin + "," + Admin;
+        public const string UserRoles = User + "," + Admin + "," + SuperAdmin;
+        public const string MentorRoles = Mentor + "," + Admin + "," + SuperAdmin;
+    }
+}
